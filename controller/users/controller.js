7ed67572b526a61d1de046a97c28a115db6ff4f8
Andrés Felipe Pérez Guizaho <andresfe52@gmail.com>
2021-10-17 15:38:24 -0500

    establecer ruta localdata

--- controller/users/controller.js
+++ controller/users/controller.js
@@ -2,6 +2,7 @@
 import { ObjectId } from 'mongodb';
 import { getDB } from '../../db/db.js';
 import jwt_decode from 'jwt-decode'
+import { response } from 'express';
 
 //MÓDULO DE USUARIOS
 const queryAllUsers = async (callback) => {
@@ -16,13 +17,28 @@ const queryAllUsers = async (callback) => {
 const queryOrCreateUser = async (req, callback)=>{
   /* 1. obtener los datos del usuario desde el token */
   const token = req.headers.authorization.split('Bearer ')[1]
-
-  console.log('token', jwt_decode(token))
+  const user = jwt_decode(token)['http://localhost/userData']
+  console.log(user)
   /* 2.  Con Auth0 verificar si el usuario ya está en la BD o no*/
+  const conexion = getDB()
+  await conexion.collection('users').findOne({ email: user.email}, async (err, response)=>{
+    console.log('Respuesta consulta BD: ', response)
+    if(response){
+      /* 3.  si el usuario ya está en BD devuelve info del usuario*/
+      callback(err, response)
+    }else{
+      /*  4. si el usuarios no está en la BD, lo crea y devulve la info*/
+      user.auth0ID = user._id
+      delete user._id
+      user.rol = 'inactivo'
+      await creacionUsuario(user, (err, respuesta) => callback(err, user))
+    }
+  })
+}
 
-  /* 3.  si el usuario ya está en BD devuelve info del usuario*/
-
-  /*  4. si el usuarios no está en la BD, lo crea y devulve la info*/
+const creacionUsuario = async (datesUsers, callback) => {
+  const conexion = getDB()
+  await conexion.collection('users').insertOne(datesUsers, callback)
 }
 
 const createUsers = async (datesUsers, callback) => {
