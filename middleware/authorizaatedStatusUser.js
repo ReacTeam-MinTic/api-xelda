27104b2ee9254405b9e3509133eb3fc4ddd6fe1f
gustavo <bocettodigital@gmail.com>
2021-10-28 22:42:38 -0500

    se corrigio la ruta de userdata que estaba como localhost

--- middleware/authorizaatedStatusUser.js
+++ middleware/authorizaatedStatusUser.js
@@ -3,7 +3,7 @@ import jwt_decode from "jwt-decode";
 
 const authorizationStatusUser = async (req, res, next) => {
   const token = req.headers.authorization.split("Bearer ")[1];
-  const user_token = jwt_decode(token)["http://localhost/userData"];
+  const user_token = jwt_decode(token)["https://fathomless-citadel-09132.herokuapp.com/userData"];
   const conexion = getDB();
   await conexion
     .collection("users")
